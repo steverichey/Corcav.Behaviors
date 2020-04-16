7552f921da63a58e8147369018cc6f4e18588ac7
Steve Richey <srichey@gowithfloat.com>
2020-04-16 09:18:53 -0500

    style: format infrastructure file

--- Corcav.Behaviors/Infrastructure.cs
+++ Corcav.Behaviors/Infrastructure.cs
@@ -1,20 +1,34 @@
+// <copyright file="Infrastructure.cs" company="Float">
+// Copyright (c) 2020 Float, All rights reserved.
+// Shared under an MIT license. See license.md for details.
+// </copyright>
+
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Corcav.Behaviors
 {
-	/// <summary>
-	/// Forces iOS linked to include Xamarin Behaviors assembly in deployed package.
-	/// </summary>
-	public static class Infrastructure
-	{
-		private static DateTime initDate;
-		public static void Init()
-		{
-			initDate = DateTime.UtcNow;
-		}
-	}
+    /// <summary>
+    /// Forces iOS linker to include Xamarin Behaviors assembly in deployed package.
+    /// </summary>
+    public static class Infrastructure
+    {
+        static DateTime initDate;
+
+        /// <summary>
+        /// Called on iOS to prevent the compiler optimizing code away.
+        /// </summary>
+        public static void Init()
+        {
+            initDate = DateTime.UtcNow;
+        }
+
+        /// <summary>
+        /// Used to avoid unused value warnings.
+        /// </summary>
+        /// <returns>The initial date.</returns>
+        public static DateTime ReadTime()
+        {
+            return initDate;
+        }
+    }
 }
